3d6a1a6ee51befddf35fff426f73083875a7dbb0
followjahidul@gmail.com <followjahidul@gmail.com>
2023-05-17 14:33:36 +0600

    fetching data from json file to overview template

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const url = require('url');
 
 const PORT = 5000;
 
+// replaceTemplate function to match with front-end placeholder 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
 // Read front-end file template  
 const tempOverview = fs.readFileSync(`${__dirname}/views/overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/views/template-card.html`, 'utf-8');
@@ -22,7 +36,9 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
-        res.end(tempOverview);
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
+        res.end(output);
     }
     else if (pathName === '/product') {
         res.writeHead(200, {
@@ -45,7 +61,6 @@ const server = http.createServer((req, res) => {
 
 
 })
-
 
 server.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
